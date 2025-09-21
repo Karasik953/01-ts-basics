acca5ecb1116e1691a4825a9aafb426ddee512b7
Karasik953 <itprogergoit@gmail.com>
2025-09-21 17:56:37 +0300

    ezdf

--- src/task-8.ts
+++ src/task-8.ts
@@ -1,16 +1,20 @@
 import axios from "axios";
 
+
 interface Post {
-    id:number,
-    title:string,
-    body:string
+  id: number;
+  title: string;
+  body: string;
 }
-async function fetchPosts() {
+
+
+async function fetchPosts(): Promise<Post[]> {
   const response = await axios.get<Post[]>(
-    '<https://jsonplaceholder.typicode.com/posts>'
+    "https://jsonplaceholder.typicode.com/posts"
   );
-  return response.data;
+  return response.data; 
 }
+
 
 fetchPosts().then((posts) => {
   console.log(posts[0].title);
